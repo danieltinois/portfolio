4b895c4c5c9a7cd6837b37c0f34729616e092d02
danieltinois <danieltinois@gmail.com>
2025-03-05 18:41:35 -0300

    CHANGE CONFIG

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,12 @@
+import SkillsAnimation from '@/components/SkillsAnimation';
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div>
+      <SkillsAnimation />
+    </div>
+  );
+};
+
+export default Skills;
---
+++ src/components/SkillsAnimation.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import React from 'react';
+
+const SkillsAnimation = () => {
+  return <div>SkillsAnimation</div>;
+};
+
+export default SkillsAnimation;
